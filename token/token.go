9377b19e4cd4403a66879122720c6671945eb725
Robin <rtmbaertschi007@gmail.com>
2024-03-28 14:27:25 +0100

    array literal

--- token/token.go
+++ token/token.go
@@ -44,10 +44,12 @@ const (
 	COMMA     = ","
 	SEMICOLON = ";"
 
-	LPAREN = "("
-	RPAREN = ")"
-	LBRACE = "{"
-	RBRACE = "}"
+	LPAREN   = "("
+	RPAREN   = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
+	LBRACKET = "["
+	RBRACKET = "]"
 
 	// Keywords
 	FUNCTION = "FUNCTION"
